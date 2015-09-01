4d8ddc15688fc1f17c2b783fe85ea085db9daf38
Halo9Pan <halo9pan@gmail.com>
2015-09-01 19:25:33 +0800

    kNN

--- python/machinelearning/kNN/basic.py
+++ python/machinelearning/kNN/basic.py
@@ -2,11 +2,11 @@
 Created on Sep 16, 2010
 kNN: k Nearest Neighbors
 
-Input:      inX: vector to compare to existing dataset (1xN)
-            dataSet: size m data set of known vectors (NxM)
-            labels: data set labels (1xM vector)
+Input:      in_x: vector to compare to existing dataset (1x_n)
+            data_set: size m data set of known vectors (Nx_m)
+            labels: data set labels (1x_m vector)
             k: number of neighbors to use for comparison (should be an odd number)
-            
+
 Output:     the most popular class label
 
 @author: pbharrin
@@ -22,12 +22,12 @@ def create_data_set():
     return group, labels
 
 def file_matrix(filename):
-    love_dictionary={'largeDoses':3, 'smallDoses':2, 'didntLike':1}
+    love_dictionary={'large_doses':3, 'small_doses':2, 'didnt_like':1}
     fr = open(filename)
     array_lines = fr.readlines()
     lines_number = len(array_lines)            #get the number of lines in the file
     return_mat = zeros((lines_number,3))       #prepare matrix to return
-    class_label_vector = []                    #prepare labels return   
+    class_label_vector = []                    #prepare labels return
     index = 0
     for line in array_lines:
         line = line.strip()
@@ -39,64 +39,64 @@ def file_matrix(filename):
             class_label_vector.append(love_dictionary.get(list_from_line[-1]))
         index += 1
     return return_mat,class_label_vector
-    
-def datingClassTest():
+
+def dating_class_test():
     ho_ratio = 0.50      #hold out 10%
-    dating_data_mat,dating_labels = file_matrix('datingTestSet2.txt')       #load data setfrom file
+    dating_data_mat,dating_labels = file_matrix('dating_test_set2.txt')       #load data setfrom file
     norm_mat, ranges, min_vals = auto_norm(dating_data_mat)
     m = norm_mat.shape[0]
     num_test_vecs = int(m*ho_ratio)
     error_count = 0.0
     for i in range(num_test_vecs):
         classifier_result = classify(norm_mat[i,:],norm_mat[num_test_vecs:m,:],dating_labels[num_test_vecs:m],3)
-        print("the classifier came back with: %d, the real answer is: %d" %(classifier_result, dating_labels[i]))
+        print(("the classifier came back with: %d, the real answer is: %d" %(classifier_result, dating_labels[i])))
         if (classifier_result != dating_labels[i]): error_count += 1.0
     print("the total error rate is: %f" %(error_count/float(num_test_vecs)))
     print(error_count)
-    
-def classifyPerson():
-    resultList = ['not at all', 'in small doses', 'in large doses']
-    percent_tats = float(raw_input(\
+
+def classify_person():
+    result_list = ['not at all', 'in small doses', 'in large doses']
+    percent_tats = float(input(\
                                   "percentage of time spent playing video games?"))
-    ffMiles = float(raw_input("frequent flier miles earned per year?"))
-    iceCream = float(raw_input("liters of ice cream consumed per year?"))
-    dating_data_mat, dating_labels = file_matrix('datingTestSet2.txt')
+    ff_miles = float(input("frequent flier miles earned per year?"))
+    ice_cream = float(input("liters of ice cream consumed per year?"))
+    dating_data_mat, dating_labels = file_matrix('dating_test_set2.txt')
     norm_mat, ranges, min_vals = auto_norm(dating_data_mat)
-    inArr = array([ffMiles, percent_tats, iceCream, ])
-    classifier_result = classify((inArr - \
+    in_arr = array([ff_miles, percent_tats, ice_cream, ])
+    classifier_result = classify((in_arr - \
                                   min_vals)/ranges, norm_mat, dating_labels, 3)
-    print("You will probably like this person: %s" %(resultList[classifier_result - 1]))
-    
+    print("You will probably like this person: %s" %(result_list[classifier_result - 1]))
+
 def img2vector(filename):
-    returnVect = zeros((1,1024))
+    return_vect = zeros((1,1024))
     fr = open(filename)
     for i in range(32):
-        lineStr = fr.readline()
+        line_str = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
-    return returnVect
+            return_vect[0,32*i+j] = int(line_str[j])
+    return return_vect
 
-def handwritingClassTest():
-    hwLabels = []
-    trainingFileList = listdir('trainingDigits')           #load the training set
-    m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
+def handwriting_class_test():
+    hw_labels = []
+    training_file_list = listdir('training_digits')           #load the training set
+    m = len(training_file_list)
+    training_mat = zeros((m,1024))
     for i in range(m):
-        fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
-    testFileList = listdir('testDigits')        #iterate through the test set
+        file_name_str = training_file_list[i]
+        file_str = file_name_str.split('.')[0]     #take off .txt
+        class_num_str = int(file_str.split('_')[0])
+        hw_labels.append(class_num_str)
+        training_mat[i,:] = img2vector('training_digits/%s' % file_name_str)
+    test_file_list = listdir('test_digits')        #iterate through the test set
     error_count = 0.0
-    mTest = len(testFileList)
-    for i in range(mTest):
-        fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifier_result = classify(vectorUnderTest, trainingMat, hwLabels, 3)
-        print("the classifier came back with: %d, the real answer is: %d" %(classifier_result, classNumStr))
-        if (classifier_result != classNumStr): error_count += 1.0
+    m_test = len(test_file_list)
+    for i in range(m_test):
+        file_name_str = test_file_list[i]
+        file_str = file_name_str.split('.')[0]     #take off .txt
+        class_num_str = int(file_str.split('_')[0])
+        vector_under_test = img2vector('test_digits/%s' % file_name_str)
+        classifier_result = classify(vector_under_test, training_mat, hw_labels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" %(classifier_result, class_num_str))
+        if (classifier_result != class_num_str): error_count += 1.0
     print("\nthe total number of errors is: %d" %(error_count))
-    print("\nthe total error rate is: %f" %(error_count/float(mTest)))
+    print("\nthe total error rate is: %f" %(error_count/float(m_test)))
--- python/machinelearning/kNN/util.py
+++ python/machinelearning/kNN/util.py
@@ -1,11 +1,11 @@
 '''
 kNN: k Nearest Neighbors
 
-Input:      inX: vector to compare to existing dataset (1xN)
-            dataSet: size m data set of known vectors (NxM)
-            labels: data set labels (1xM vector)
+Input:      in_x: vector to compare to existing dataset (1x_n)
+            data_set: size m data set of known vectors (Nx_m)
+            labels: data set labels (1x_m vector)
             k: number of neighbors to use for comparison (should be an odd number)
-            
+
 Output:     the most popular class label
 
 @author: pbharrin
@@ -16,16 +16,16 @@ from os import listdir
 
 def classify(in_x, data_set, labels, k):
     data_set_size = data_set.shape[0]
-    diff_mat = tile(in_x, (data_set_size,1)) - data_set
+    diff_mat = tile(in_x, (data_set_size,1)) - data_set # tile重复数组，后一参数为维度
     sq_diff_mat = diff_mat**2
     sq_distances = sq_diff_mat.sum(axis=1)
     distances = sq_distances**0.5
-    sorted_dist_indicies = distances.argsort()     
-    class_count={}          
+    sorted_dist_indicies = distances.argsort() # array排序，返回排序后的index
+    class_count={}
     for i in range(k):
         vote_ilabel = labels[sorted_dist_indicies[i]]
         class_count[vote_ilabel] = class_count.get(vote_ilabel,0) + 1
-    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sorted_class_count = sorted(iter(class_count.items()), key=operator.itemgetter(1), reverse=True)
     return sorted_class_count[0][0]
 
 def auto_norm(data_set):
